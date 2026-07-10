1905fd5f84f853875c3785eb1ed433d304a0a4e3
Greek Kumar <149527008+Greek1606@users.noreply.github.com>
2026-07-10 19:26:06 +0530

    Time: 0 ms (100%), Space: 11.1 MB (82.24%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,23 +1,27 @@
-#include<bits/stdc++.h>
 class Solution {
 public:
-    bool check(vector<int>& nums) {
-        int j=1;
-        while(j<nums.size()){
-            if(nums[j-1]<=nums[j]) {
-                j++;
-                continue;
-            };
-            reverse(nums.begin(),nums.begin()+j);
-            reverse(nums.begin()+j,nums.end());
-            reverse(nums.begin(),nums.end());
-            break;
+    bool rotate(vector<int>& nums,int k) {
+        reverse(nums.begin(),nums.end()-k);
+        reverse(nums.end()-k,nums.end());
+        reverse(nums.begin(),nums.end());
+        for(int i=1;i<nums.size();i++) {
+            if(nums[i-1]>nums[i]) return false;
         }
-        if(nums.size()<=j) j %=nums.size();
-        if(!is_sorted(nums.begin(),nums.end())) return false;
         return true;
-        
 
+
+    }
+    bool check(vector<int>& nums) {
+        int rot =0;
+        for(int i=1;i<nums.size();i++) {
+            if(nums[i-1]>nums[i]) {
+                rot = nums.size()-i;
+                break;
+            }
+        }
+        if(rot==0) return true;
+        // if(rot>nums.size()) rot %=nums.size();
+        return rotate(nums,rot);
         
     }
 };
